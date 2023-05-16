8c972db8f1738aaf4bdf02d7f586ba6b63a67c28
LuisPassosRamos <129339010+LuisPassosRamos@users.noreply.github.com>
2023-05-16 16:28:03 +0000

    changes

---
+++ repetição/q1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+//Faça um programa que leia um número inteiro positivo N  
+//e imprima todos os números naturais de 0 até N em ordem crescente.
+int main (){
+    int n;
+    printf ("Digite um número positivo:");
+    scanf ("%d", n);
+
+    for (int i = 0; i < n; i++){
+        printf ("%d \n", i);
+    }
+    
+    return 0;
+}
