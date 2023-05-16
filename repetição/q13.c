b5876bb8d65dcbbcf177fcf33dc05ab62d9c3514
LuisPassosRamos <129339010+LuisPassosRamos@users.noreply.github.com>
2023-05-16 21:27:15 +0000

    changes

---
+++ repetição/q13.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Faça um programa que exiba a soma de todos os números naturais abaixo de 1.000 que são múltiplos de 3 ou 5.
+
+int main()
+{
+    int num, soma;
+    for (int i = 0; i < 1000; i++)
+    {
+        if ((i % 3) || == 0)
+    }
+    
+    return 0;
+}
