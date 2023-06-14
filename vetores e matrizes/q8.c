34e4516830858f94c77df063f55f77c0bad3f02f
LuisPassosRamos <129339010+LuisPassosRamos@users.noreply.github.com>
2023-06-14 23:09:29 +0000

    changes

---
+++ vetores e matrizes/q8.c
@@ -0,0 +1,29 @@
+//Calcular e imprimir na tela uma matriz de tamanho 10 × 10, em que seus elementos são da forma:
+/* A[i][j] = 2i + 7j - 2 (se i<j)
+   A[i][j] = 3i^2-1 (se i=j)
+   A[i][j] = 4i^3 + 5j^2 + 1 (se i>j)*/
+
+   #include <stdio.h>
+   #include <stdlib.h>
+
+   int main(void)
+   {
+    int matriz [10][10];
+
+    for (int i = 0; i < count; i++)
+    {
+        for (int j = 0; j < count; j++)
+        {
+            if ()
+            {
+                /* code */
+            }
+            
+            matriz[i][j] 
+        }
+        
+    }
+    
+    
+   }
+   
