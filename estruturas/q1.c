27ac55d5c434a4a505446c06d06781509b5d653b
LuisPassosRamos <129339010+LuisPassosRamos@users.noreply.github.com>
2023-07-12 23:17:07 +0000

    changes

--- estruturas/q1.c
+++ estruturas/q1.c
@@ -1,17 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (void){
-struct ind
+struct birthday
 {
-    char nome, endereço;
-    int idade;
+    int day;
+    int month;
+    int year;
+};
+
+struct address
+{
+    char street[30];
+    char district[30];
+    int number;
+};
+
+struct register1
+{
+    char name[30];
+    struct birthday date;
+    struct address home;
+};
+
+struct register1 subject(void){
+    struct register1 user;
+    printf("Digite seu nome:\n");
+    setbuf(stdin, NULL);
+    gets(user.name);
+
+    printf("Digite o dia do seu nascimento:\n");
+    scanf("%d", &user.date.day);
+    printf("Digite o mês do seu nascimento:\n");
+    scanf("%d", &user.date.month);
+    printf("Digite o ano do seu nascimento:\n");
+    scanf("%d", &user.date.year);
+
+    printf("Digite sua rua:\n");
+    setbuf(stdin, NULL);
+    gets(user.home.street);
+    printf("Digite seu bairro:\n");
+    setbuf(stdin, NULL);
+    gets(user.home.district);
+    printf("Digite o número da casa:\n");
+    scanf("%d", &user.home.number);
+    return (user);
 }
-struct ind user;
-printf ("Digite seu nome:\n");
-scanf ("%c", &user.nome);
-printf ("Digite sua idade:\n");
-scanf ("%c", &user.idade);
-printf ("Digite o seu endereço:\n ");
-scanf ("%c", &user.endereço);
+
+
+void main()
+{
+    
+    subject();
+    printf("O nome do usuário é: %s \n", user.name);
+    printf("A data do nascimento do usuário é: %d/%d/%d \n", user.date.day, user.date.month, user.date.year);
+    printf("A rua do usuário é: %s \n", user.home.street);
+    printf("O bairro do usuário é: %s \n", user.home.district);
+    printf("O número da casa do usuário é: %d \n", user.home.number);
 }
