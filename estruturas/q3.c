4b553b3cc8ee5dfef009e43d05a042e6019cc1f6
LuisPassosRamos <129339010+LuisPassosRamos@users.noreply.github.com>
2023-07-26 19:00:43 +0000

    changes

--- estruturas/q3.c
+++ estruturas/q3.c
@@ -10,22 +10,26 @@ struct pontos
     int x;
     int y;
 };
-struct pontos distância(struct pontos coordenada[0], struct pontos coordenada[1])
+
+float distancia(struct pontos coordenada0, struct pontos coordenada1)
 {
-    return (sqrt(pow((coordenada[0].x - coordenada[0].y), 2) + pow((coordenada[1].x - coordenada[1].y), 2)))
+    float dist = (sqrt(pow((coordenada0.x - coordenada1.x), 2) + pow((coordenada0.y - coordenada1.y), 2)));
+    return dist;
 }
 
-void main()
+int main()
 {
     struct pontos coordenada[2];
     printf ("Informe os pontos da primeira coordenada: (x,y) \n");
     scanf ("%d %d", &coordenada[0].x, &coordenada[0].y);
 
-    printf ("Informe os pontos da primeira coordenada: (x,y) \n");
+    printf ("Informe os pontos da segunda coordenada: (x,y) \n");
     scanf ("%d %d", &coordenada[1].x, &coordenada[1].y);
 
     printf ("primeiro x %d primeiro y %d \n", coordenada[0].x, coordenada[0].y);
     printf ("segundo x %d segundo y %d \n", coordenada[1].x, coordenada[1].y);
 
+    printf ("A distancia entre os pontos é: %f \n", distancia(coordenada[0], coordenada[1]));
     
+    return 0;
 }
