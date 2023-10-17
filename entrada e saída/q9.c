6a9fe79dc6ed766faa76207979da349855a3055e
LuisPassosRamos <20222tadssaj0015@ifba.edu.br>
2023-10-17 10:44:51 -0300

    changes

--- entrada e saída/q9.c
+++ entrada e saída/q9.c
@@ -1,8 +1,18 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
 //Ler dois inteiros (variáveis A e B) e imprimir o resultado do quadrado da diferença do primeiro valor pelo segundo.
-int main()
-{
-    int a, b;
+int main() {
+    int A, B;
+
+    printf("Digite o valor de A: ");
+    scanf("%d", &A);
+
+    printf("Digite o valor de B: ");
+    scanf("%d", &B);
+
+    int diferenca = A - B;
+    int resultado = diferenca * diferenca;
+
+    printf("O quadrado da diferença entre A e B é: %d\n", resultado);
+
     return 0;
 }
