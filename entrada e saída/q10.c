c9c2181502f90d5073e192caf521ba5023afa620
LuisPassosRamos <20222tadssaj0015@ifba.edu.br>
2023-10-17 11:00:56 -0300

    changes

--- entrada e saída/q10.c
+++ entrada e saída/q10.c
@@ -1,53 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-/*Efetuar o cálculo da quantidade de litros de combustível gasta em uma viagem,
-utilizando um automóvel que faz 12 Km por litro.*/
-struct tempo
-{
-    int horas;
-    int minutos;
-    int segundos;
-};
-double distancia(double velocidade, double tempo_viagem)
-{
-    //Retornando o valor do cálculo da distância percorrida;
-    return (velocidade*tempo_viagem);
-}
-double litros_usados(double distancia)
-{
-    double km_l;
+#include<stdio.h>
+#include<stdlib.h>
+/*Elaborar um programa que efetue a apresentação do valor da conversão em real de um valor lido em dólar.
+O programa deve solicitar o valor da cotação do dólar e também a quantidade de dólares disponível com o usuário,
+para que seja apresentado o valor em moeda brasileira.*/
+int main(){
+    float real;
+    printf ("Digite o valor para conversão em reais: ");
+    scanf("%f", &real);
+    float dolar = (5.06*real);
 
-    printf ("Informe quantos litros por quilômetro seu veículo consome:\n");
-
-    printf ("%f", &km_l);
-    //Efetuando e retornando cálculo dos litros usados durante a viagem;
-    return (distancia/km_l);
-}
-
-int main ()
-{
-    //Declaração da variável do tempo de viagem e a variável da velocidade média;
-    struct tempo viagem;
-
-    double velocidade;
-    //Solicitando o valor do tempo de viagem e o valor da velocidade média durante a viagem;
-    printf("Digite o tempo gasto em viagem: [hh:mm:ss]\n");
-
-    scanf ("%d:%d:%d", &viagem.horas, &viagem.minutos, &viagem.segundos);
-
-    printf("Digite a velocidade média em km/h durante a viagem:\n");
-
-    scanf ("%f", &velocidade);
-
-    //Declarando e convertendo o tempo da viagem pra horas;
-    double tempo_viagem = (viagem.horas + (viagem.minutos/60) + (viagem.segundos/3600));
-
-    //Declarando a variável de litros por quilômetro;
-    double dist = distancia(velocidade, tempo_viagem);
-    double lit = litros_usados(dist);
-
-    ///Retornando o valor da velocidade média, do tempo gasto, da distancia percorrida e dos litros usados.
-    printf("A velocidade média é de %.2f\nO tempo gasto na viagem foi de %.2f\nA distância percorrida foi de %.2f\nOs litros usados na viagem foram %.2f", velocidade, tempo_viagem, dist, lit);
+    printf("O valor convertido de reais para dólares é: %.2f", dolar);
 
     return 0;
 }
--- entrada e saída/q9.c
+++ entrada e saída/q9.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 //Ler dois inteiros (variáveis A e B) e imprimir o resultado do quadrado da diferença do primeiro valor pelo segundo.
 int main() {
+    //Declarando variáveis;
     int A, B;
-
+    //Solicitando e guardando o valor das variáveis;
     printf("Digite o valor de A: ");
     scanf("%d", &A);
 
     printf("Digite o valor de B: ");
     scanf("%d", &B);
-
+    //Cálculo da diferença e armazenamento do resultado;
     int diferenca = A - B;
     int resultado = diferenca * diferenca;
-
+    //Mostrando resutado ao usuário;
     printf("O quadrado da diferença entre A e B é: %d\n", resultado);
 
     return 0;
