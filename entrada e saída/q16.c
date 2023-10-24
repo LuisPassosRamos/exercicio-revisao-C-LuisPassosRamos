74f32a143392d4f73791e1082100e7247ca24342
LuisPassosRamos <20222tadssaj0015@ifba.edu.br>
2023-10-23 21:42:59 -0300

    changes

---
+++ entrada e saída/q16.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/*Em uma eleição sindical concorreram ao cargo de presidente três candidatos (A, B e C).
+Durante a apuração dos votos foram computados votos nulos e votos em branco,
+além dos votos válidos para cada candidato.
+Deve ser criado um programa de computador que efetue a leitura da quantidade de votos válidos para cada candidato,
+além de efetuar também a leitura da quantidade de votos nulos e votos em branco.
+Ao final o programa deve apresentar*/
+int main(){
+    int A, B, C;
+
+
+
+
+    return 0;
+}
