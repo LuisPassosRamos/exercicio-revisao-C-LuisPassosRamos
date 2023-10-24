ea0905fff6dddcfee96c3efa19a366d6f8178350
LuisPassosRamos <20222tadssaj0015@ifba.edu.br>
2023-10-24 20:05:04 -0300

    changes

--- entrada e saída/q16.c
+++ entrada e saída/q16.c
@@ -7,16 +7,20 @@ além de efetuar também a leitura da quantidade de votos nulos e votos em branco.
 Ao final o programa deve apresentar*/
 int main()
 {
+    //Declaracao das variaveis;
     int A = 0;
     int B = 0;
     int C = 0;
     int votosNulos = 0;
     int votosBrancos = 0;
     int keyb;
+    //Estrutura de repeticao para solicitar o voto e fazer o teste logico;
     for (int i = 0; i < 10; i++)
     {
+        //Solicitando e guardando o voto do usuario;
         printf("Digite o numero do candidato \n(A=1, B=2, C=3, Branco=0)\n");
         scanf("%d", &keyb);
+        //Teste logico para incrementacao das variaveis dos cadidatos;
         switch (keyb)
         {
         case 1:
@@ -36,6 +40,7 @@ int main()
             break;
         }
     }
+    //Imprimindo todos os votos ao usuario.
     printf("Votos no candidato A: %d\nVotos no candidato B: %d\nVotos no candidato C: %d\nVotos brancos: %d\nVotos nulos: %d", A, B, C, votosBrancos, votosNulos);
     return 0;
 }
