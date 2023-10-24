77de3479569595db243bdeae00080e8ae83be3e7
LuisPassosRamos <20222tadssaj0015@ifba.edu.br>
2023-10-24 19:45:51 -0300

    changes

--- entrada e saída/q16.c
+++ entrada e saída/q16.c
@@ -5,11 +5,37 @@ além dos votos válidos para cada candidato.
 Deve ser criado um programa de computador que efetue a leitura da quantidade de votos válidos para cada candidato,
 além de efetuar também a leitura da quantidade de votos nulos e votos em branco.
 Ao final o programa deve apresentar*/
-int main(){
-    int A, B, C;
-
-
-
-
+int main()
+{
+    int A = 0;
+    int B = 0;
+    int C = 0;
+    int votosNulos = 0;
+    int votosBrancos = 0;
+    int keyb;
+    for (int i = 0; i < 10; i++)
+    {
+        printf("Digite o numero do candidato \n(A=1, B=2, C=3, Branco=0)\n");
+        scanf("%d", &keyb);
+        switch (keyb)
+        {
+        case 1:
+            A++;
+            break;
+        case 2:
+            B++;
+            break;
+        case 3:
+            C++;
+            break;
+        case 0:
+            votosBrancos++;
+            break;
+        default:
+            votosNulos++;
+            break;
+        }
+    }
+    printf("Votos no candidato A: %d\nVotos no candidato B: %d\nVotos no candidato C: %d\nVotos brancos: %d\nVotos nulos: %d", A, B, C, votosBrancos, votosNulos);
     return 0;
 }
--- repetição/q2.c
+++ repetição/q2.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-//Faça um programa que leia um número inteiro positivo N 
+//Faça um programa que leia um número inteiro positivo N
 //e imprima todos os números naturais de 0 até N em ordem decrescente.
 int main (){
     int n;
     printf ("Digite um número positivo:");
     scanf ("%d", &n);
 
-    for (n ;n >= 0; n--){
+    for (n; n >= 0; n--){
         printf ("%d \n", n);
     }
 
