da1280617e32b6488a6b72f76f6470e9aab325a3
LuisPassosRamos <20222tadssaj0015@ifba.edu.br>
2023-10-31 17:50:58 -0300

    changes

--- listas_sequenciais/lista_estatica_cadastro.c
+++ listas_sequenciais/lista_estatica_cadastro.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 #define MAX_SIZE 10
 
@@ -17,25 +17,25 @@ typedef struct
     int size;
 } list;
 
-void initializeList(list* list)
+void initializeList(list *list)
 {
     list->size = 0;
 }
 
-bool insert(list* list, data data)
+bool insert(list *list, data data)
 {
     if (list->size >= MAX_SIZE)
     {
         return false;
     }
 
-    list->data[size] = data;
+    list->data[list->size] = data;
     list->size++;
 
     return true;
 }
 
-bool insertPos(list* list, data data, int pos)
+bool insertPos(list *list, data data, int pos)
 {
     if (list->size >= MAX_SIZE || pos > list->size || pos < 0)
     {
@@ -49,13 +49,12 @@ bool insertPos(list* list, data data, int pos)
         list->data[i] = list->data[i - 1];
     }
 
-
     list->data[pos] = data;
 
     return true;
 }
 
-void copyList (list list1, list* list2)
+void copyList(list list1, list *list2)
 {
     for (int i = 0; i < list1.size; i++)
     {
@@ -74,7 +73,7 @@ bool areEqual(list list1, list list2)
 
     for (int i = 0; i < list1.size; i++)
     {
-        if (list1.data[i] == list2.data[i])
+        if (list1.data[i].age == list2.data[i].age && list1.data[i].height == list2.data[i].height && list1.data[i].ID == list2.data[i].ID)
         {
             return true;
         }
@@ -83,35 +82,52 @@ bool areEqual(list list1, list list2)
     return false;
 }
 
-void removeData(list* list, data data){
-    for (int i = 0; i < list->size; i++){
-        if (list->data[i] == data){
-            for(int j = list->size; j > i; j--){
-                list->data[j-1] = list->data[j];
+void removeData(list *list, data data)
+{
+    for (int i = 0; i < list->size; i++)
+    {
+        if (list->data[i].age == data.age && list->data[i].height == data.height && list->data[i].ID == data.ID)
+        {
+            for (int j = list->size; j > i; j--)
+            {
+                list->data[j - 1] = list->data[j];
             }
             list->size--;
         }
     }
 }
 
-void printList(list list1){
-    for (int i = 0; i < list1.size; i++){
-        if()
-        printf("Age: %d \n%f %d")
+void printList(list list1)
+{
+    for (int i = 0; i < list1.size; i++)
+    {
+        if (i != 0)
+            printf("\n");
+        printf("Age: %d\nHeight: %.2f\nID: %d", list1.data[i].age, list1.data[i].height, list1.data[i].ID);
     }
-    
 }
 
-int main(){
+int main()
+{
     list myList;
 
-    for(int i = 0; i < 10; i++){
+    initializeList(&myList);
+
+    for (int i = 0; i < 10; i++)
+    {
         data temp;
-        temp.age = i+1;
-        temp.height = i+1;
-        temp.ID = i+1;
-        mylist.data[i] = temp;
+        temp.age = i + 1;
+        temp.height = i + 1;
+        temp.ID = i + 1;
+        insert(&myList, temp);
     }
 
-    
+    data temp;
+    temp.age = 9;
+    temp.height = 9;
+    temp.ID = 9;
+    removeData(&myList, temp);
+    printList(myList);
+
+    return 0;
 }
